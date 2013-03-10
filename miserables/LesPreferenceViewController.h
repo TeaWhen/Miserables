6cb9451659314747c30551dc508ee059a0ee8083
Xhacker Liu <liu.dongyuan@gmail.com>
2013-03-10 18:01:59 +0800

    add empty preference page.

---
+++ miserables/LesPreferenceViewController.h
@@ -0,0 +1,13 @@
+//
+//  LesPreferenceViewController.h
+//  miserables
+//
+//  Created by Xhacker on 2013-03-10.
+//  Copyright (c) 2013 Eksband. All rights reserved.
+//
+
+#import "QuickDialogController.h"
+
+@interface LesPreferenceViewController : QuickDialogController
+
+@end
---
+++ miserables/LesPreferenceViewController.m
@@ -0,0 +1,38 @@
+//
+//  LesPreferenceViewController.m
+//  miserables
+//
+//  Created by Xhacker on 2013-03-10.
+//  Copyright (c) 2013 Eksband. All rights reserved.
+//
+
+#import "LesPreferenceViewController.h"
+
+@interface LesPreferenceViewController ()
+
+@end
+
+@implementation LesPreferenceViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
