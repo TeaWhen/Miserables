1a2c6e240112ee5c8e120f33542c9e5b20a499df
Xhacker Liu <liu.dongyuan@gmail.com>
2013-03-15 20:04:21 +0800

    basic search.

--- miserables/LesSearchViewController.m
+++ miserables/LesSearchViewController.m
@@ -11,13 +11,15 @@
 #import "LesNavigationController.h"
 #import "FMResultSet.h"
 
-@interface LesSearchViewController () <UISearchBarDelegate>
+@interface LesSearchViewController () <UISearchBarDelegate, UITableViewDataSource>
 
 @property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
 @property (weak, nonatomic) IBOutlet UITableView *resultTableView;
 
 @property (weak, nonatomic) LesViewController *parent;
 @property (weak, nonatomic) LesNavigationController *nav;
+
+@property (strong, nonatomic) NSMutableArray *result;
 
 @end
 
@@ -29,9 +31,14 @@
     
     self.nav = (LesNavigationController *)(self.presentingViewController);
     self.parent = [self.nav.viewControllers objectAtIndex:0];
+    
+    self.result = [[NSMutableArray alloc] init];
 	
     self.searchBar.delegate = self;
+    self.resultTableView.dataSource = self;
     [self.searchBar becomeFirstResponder];
+    
+    [self searchBar:self.searchBar textDidChange:@""];
 }
 
 - (void) searchBarSearchButtonClicked:(UISearchBar*) searchBar
@@ -51,16 +58,46 @@
 - (void) searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText
 {
     FMResultSet *articles = [self.nav.db executeQuery:@"SELECT * FROM Article WHERE title LIKE ?", [NSString stringWithFormat:@"%@%%", searchText]];
+    [self.result removeAllObjects];
     while ([articles next]) {
         NSString *title = [articles stringForColumn:@"title"];
+        [self.result addObject:title];
         NSLog(@" -> %@", title);
     }
+    [self.resultTableView reloadData];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark Table view methods
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+// Customize the number of rows in the table view.
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.result.count;
+}
+
+// Customize the appearance of table view cells.
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] init];
+    }
+    
+	cell.textLabel.text = [self.result objectAtIndex:indexPath.row];
+    return cell;
 }
 
 @end
