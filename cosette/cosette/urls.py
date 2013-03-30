41e808c007f5b4ee04216bb63a00ec293ca5f77a
Zheng Yan <yzheng624@gmail.com>
2013-03-30 16:00:14 +0800

    deploy on 42.

--- cosette/cosette/urls.py
+++ cosette/cosette/urls.py
@@ -1,9 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
     (r'^app/version/$', 'miserables.views.version'),
 )
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
