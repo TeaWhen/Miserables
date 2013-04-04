0ec7307221094e82e365a916ed92540e0d69584d
Zheng Yan <yzheng624@gmail.com>
2013-04-04 21:02:01 +0800

    use unicode to display.

--- cosette/miserables/models.py
+++ cosette/miserables/models.py
@@ -6,18 +6,18 @@ class App(models.Model):
     db = models.CharField(max_length=30)
     
     def __unicode__(self):
-        return 'Beta: {}  -  Stable: {} - Db: {}'.format(self.version_beta, self.version_stable, self.db)
+        return u'Beta: {}  -  Stable: {} - Db: {}'.format(self.version_beta, self.version_stable, self.db)
 
 class Notice(models.Model):
     time = models.DateTimeField(auto_now=True)
     content = models.TextField()
 
     def __unicode__(self):
-        return 'Time: {} - Content: {}'.format(self.time, self.content)
+        return u'Time: {} - Content: {}'.format(self.time, self.content)
 
 class Library(models.Model):
     time = models.DateTimeField(auto_now=True)
     url = models.CharField(max_length=300)
 
     def __unicode__(self):
-        return 'Id: {} - Time: {} - Url: {}'.format(self.pk, self.time, self.url)
+        return u'Id: {} - Time: {} - Url: {}'.format(self.pk, self.time, self.url)
