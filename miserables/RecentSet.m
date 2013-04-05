f99a5446d29bb1ff84bec7cbb10e35c48cdeccec
Xhacker Liu <liu.dongyuan@gmail.com>
2013-04-05 15:30:44 +0800

    rename. back and forward seems doesn't work? @yzheng624

--- miserables/RecentSet.m
+++ miserables/RecentSet.m
@@ -13,7 +13,7 @@
 @interface RecentSet ()
 
 @property FMDatabase *DB;
-@property NSMutableArray *recent;
+@property NSMutableArray *recents;
 - (void)openDB;
 - (void)reload;
 
@@ -68,26 +68,26 @@
 
 - (NSMutableArray *)list
 {
-    if (!self.recent) {
+    if (!self.recents) {
         [self reload];
     }
     
-    return self.recent;
+    return self.recents;
 }
 
 - (void)reload
 {
-    FMResultSet *rs = [self.DB executeQuery:@"SELECT * FROM Recent ORDER BY timestamp"];
-    self.recent = [[NSMutableArray alloc] init];
+    FMResultSet *rs = [self.DB executeQuery:@"SELECT * FROM Recents ORDER BY timestamp"];
+    self.recents = [[NSMutableArray alloc] init];
     while ([rs next]) {
         NSString *title = [rs stringForColumn:@"title"];
-        [self.recent addObject:title];
+        [self.recents addObject:title];
     }
 }
 
 - (BOOL)exist:(NSString *)title
 {
-    FMResultSet *s = [self.DB executeQuery:@"SELECT * FROM Recent WHERE title = ?", title];
+    FMResultSet *s = [self.DB executeQuery:@"SELECT * FROM Recents WHERE title = ?", title];
     if ([s next]) {
         return YES;
     }
@@ -97,30 +97,30 @@
 - (void)add:(NSString *)title
 {
     NSTimeInterval timestamp = [[NSDate date] timeIntervalSince1970];
-    [self.DB executeUpdate:@"INSERT INTO Recent (title, timestamp) VALUES (?, ?)", title, [NSString stringWithFormat:@"%d", (NSInteger)timestamp]];
+    [self.DB executeUpdate:@"INSERT INTO Recents (title, timestamp) VALUES (?, ?)", title, [NSString stringWithFormat:@"%d", (NSInteger)timestamp]];
     [self reload];
 }
 
 - (void)update:(NSString *)title
 {
     NSTimeInterval timestamp = [[NSDate date] timeIntervalSince1970];
-    [self.DB executeQuery:@"UPDATE Recent SET timestamp = ? WHERE title = ?", [NSString stringWithFormat:@"%d", (NSInteger)timestamp], title];
+    [self.DB executeQuery:@"UPDATE Recents SET timestamp = ? WHERE title = ?", [NSString stringWithFormat:@"%d", (NSInteger)timestamp], title];
     [self reload];
 }
 
 - (void)remove:(NSString *)title
 {
-    [self.DB executeUpdate:@"DELETE FROM Recent WHERE title = ?", title];
+    [self.DB executeUpdate:@"DELETE FROM Recents WHERE title = ?", title];
     [self reload];
 }
 
 - (NSInteger)count
 {
-    if (!self.recent) {
+    if (!self.recents) {
         [self list];
     }
     
-    return self.recent.count;
+    return self.recents.count;
 }
 
 @end
