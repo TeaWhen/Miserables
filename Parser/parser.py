57f21ecc1d1c0a49da972268869cfae2b3da0ffb
Zheng Yan <yzheng624@gmail.com>
2013-04-20 21:02:46 +0800

    test.

--- Parser/parser.py
+++ Parser/parser.py
@@ -9,19 +9,23 @@ import os
 import os.path
 from bs4 import BeautifulSoup
 import zlib
+from threading import Thread
+from Queue import Queue
+import apsw
 
-conn = sqlite3.connect('articles.db')
+conn = apsw.connect('articles.db')
 conn.text_factory = str
-c = conn.cursor()
+cursor = conn.cursor()
 
 def open_db():
 	c.execute('CREATE TABLE IF NOT EXISTS Articles (title TEXT, content BLOB)')
+	self.start()
 
-def insert_article(title, content):
+def insert_article(self, title, content):
 	compressed = zlib.compress(content)
 	c.execute('INSERT INTO Articles (title, content) VALUES (?, ?)', (title, sqlite3.Binary(compressed)))
-
-def parse(html):
+	
+def parse(self, html):
 	soup = BeautifulSoup(html)
 	html = soup.body.find(id='mw-content-text')
 	scr = html.find_all('script')
@@ -45,7 +49,7 @@ def parse(html):
 			del tag['rel']
 		if 'title' in tag.attrs:
 			del tag['title']
-	
+		
 	# delete id="noteTA"'s div
 	tags = html.find_all('div', id='noteTA')
 	for tag in tags:
@@ -79,36 +83,45 @@ def parse(html):
 	html = re.sub(r'<!--.+?-->', '', html, 0, re.DOTALL)
 	return html
 
-def main():
-	open_db()
-	f = open('zhwiki-latest-all-titles-in-ns0', 'r')
-	res = c.execute('SELECT title FROM Articles')
-	titles = [title[0] for title in res.fetchall()]
-	count = 0
-	for title in f:
-		title = title.strip()
-		count = count + 1
-		print count, title
-		if title in titles:
-			continue
+def run():
+	while True:
+		item = q.get()
+		title = item[1]
+		
+		print item[0], title
+
 		headers = {'User-agent': 'Mozilla/5.0'}
 		r = requests.get('http://zh.wikipedia.org/w/index.php?title={0}&variant=zh-cn&redirect=no'.format(urllib.quote(title)), headers=headers)
 		html = parse(r.text)
-
+	
 		# output HTML file for debug
-		output_dir = os.path.join(os.path.dirname(__file__), 'contents')
-		if not os.path.exists(output_dir):
-			os.makedirs(output_dir)
-		with open(os.path.join(output_dir, title.replace('/', '-') + '.html'), 'w') as html_file:
-			html_file.write(html)
-
+		# output_dir = os.path.join(os.path.dirname(__file__), 'contents')
+		# if not os.path.exists(output_dir):
+		# 	os.makedirs(output_dir)
+		# with open(os.path.join(output_dir, title.replace('/', '-') + '.html'), 'w') as html_file:
+		# 	html_file.write(html)
+		
 		insert_article(title, html)
-		if count % 10000 == 0:
-			conn.commit()
+		q.task_done()
+
+q = Queue()
+
+def main():
+	f = open('zhwiki-latest-all-titles-in-ns0', 'r')
+	res = cursor.execute('SELECT title FROM Articles')
+	titles = [title[0] for title in res.fetchall()]
+	
+	for i in range(10):
+		t = Thread(target=run)
+		t.daemon = True
+		t.start()
+
+	counter = 0
+	for title in f:
+		title = title.strip()
+		counter += 1
+		if title not in titles:
+			q.put([counter, title])
 
 if __name__ == "__main__":
-	try:
-		main()
-		conn.commit()
-	except KeyboardInterrupt:
-		conn.commit()
+	main()
